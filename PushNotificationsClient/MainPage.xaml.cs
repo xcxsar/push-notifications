7f322eebec9002821b178033a363a9df29c6d9a5
xcxsar <l2225010052@guasave.tecnm.mx>
2025-09-08 21:29:47 -0700

    Add Firebase Cloud Messaging support and UI updates
    - Removed welcome label and added notification entry in MainPage.xaml.
    - Updated MainPage.xaml.cs to retrieve and display Firebase token.
    - Registered Firebase services in MauiProgram.cs for Android.
    - Modified AndroidManifest.xml to include necessary Firebase receivers.
    - Enhanced MainActivity.cs to handle intents and create notification channel.
    - Created strings.xml for Firebase Crashlytics mapping file ID.
    - Updated PushNotificationsClient.csproj to target Android framework and include Firebase dependency.

--- PushNotificationsClient/MainPage.xaml.cs
+++ PushNotificationsClient/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace PushNotificationsClient
+﻿using Plugin.Firebase.CloudMessaging;
+
+namespace PushNotificationsClient
 {
     public partial class MainPage : ContentPage
     {
@@ -9,16 +11,11 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+        private async void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times !";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            await CrossFirebaseCloudMessaging.Current.CheckIfValidAsync();
+            var token = await CrossFirebaseCloudMessaging.Current.GetTokenAsync();
+            NotificationEntry.Text = token;
         }
     }
 }
--- PushNotificationsClient/MauiProgram.cs
+++ PushNotificationsClient/MauiProgram.cs
@@ -1,4 +1,10 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+
+
+#if ANDROID
+using Plugin.Firebase.Core.Platforms.Android;
+#endif
 
 namespace PushNotificationsClient
 {
@@ -21,5 +27,18 @@ namespace PushNotificationsClient
 
             return builder.Build();
         }
+
+        private static MauiAppBuilder RegisterFirebaseServices(this MauiAppBuilder builder)
+        {
+            builder.ConfigureLifecycleEvents(events => {
+#if ANDROID
+                events.AddAndroid(android => android.OnCreate((activity, _) =>
+                CrossFirebase.Initialize(activity)));
+#endif
+            });
+
+            return builder;
+        }
+
     }
 }
