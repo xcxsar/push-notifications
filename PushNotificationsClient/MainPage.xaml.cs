10cb1cdfeec1e893258bed3f1113eaff663cb798
xcxsar <l2225010052@guasave.tecnm.mx>
2025-09-09 14:43:06 -0700

    Implement Push Notifications Server with Azure Functions and update client UI

--- PushNotificationsClient/MainPage.xaml.cs
+++ PushNotificationsClient/MainPage.xaml.cs
@@ -4,18 +4,31 @@ namespace PushNotificationsClient
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
+        public DateTime MaxDate { get; } = DateTime.Today.AddMonths(3);
+        public TimeSpan SelectedTime { get; set; } = new TimeSpan(12, 0, 0);
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
         }
 
-        private async void OnCounterClicked(object sender, EventArgs e)
+        private async void GetFCMToken(object sender, EventArgs e)
         {
-            await CrossFirebaseCloudMessaging.Current.CheckIfValidAsync();
-            var token = await CrossFirebaseCloudMessaging.Current.GetTokenAsync();
-            NotificationEntry.Text = token;
+            try
+            {
+                await CrossFirebaseCloudMessaging.Current.CheckIfValidAsync();
+                var token = await CrossFirebaseCloudMessaging.Current.GetTokenAsync();
+                Console.WriteLine(token);
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
+
+        private async void OnScheduleAppointmentClicked(object sender, EventArgs e)
+        {
+            await CrossFirebaseCloudMessaging.Current.SubscribeToTopicAsync("Appointments");
         }
     }
 }
